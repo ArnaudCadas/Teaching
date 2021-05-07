95f44042cb3afb53da65dc77d67a3fd2c39bfb7f
ArnaudCadas <cadas.arnaud@gmail.com>
2021-05-07 10:50:16 +0200

    new multiply function

--- src/moduleA.py
+++ src/moduleA.py
@@ -4,3 +4,8 @@ import numpy as np
 def sum_arrays(a, b):
     print(f'Sum of {a} and {b}')
     return a + b
+
+
+def multiply_arrays(a, b):
+    print(f'Multiply {a} with {b}')
+    return np.multiply(a, b)
