d4acaafd33328e49bbcaa6992f5bccfd8956b124
ArnaudCadas <cadas.arnaud@gmail.com>
2021-05-12 16:18:13 +0200

    fix test

--- __test__/test_moduleA.py
+++ __test__/test_moduleA.py
@@ -6,8 +6,9 @@ from src.moduleA import sum_arrays
 def test_sum_arrays():
     a = np.array([1., 2.])
     b = np.array([3., 4.])
-    expected = np.array([4., 6.])
+    c = np.array([0., -1.])
+    expected = np.array([4., 5.])
 
-    res = sum_arrays(a=a, b=b)
+    res = sum_arrays(a=a, b=b, c=c)
 
     assert np.all(res == expected)
